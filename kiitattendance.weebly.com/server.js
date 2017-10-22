a87f7a2281df1c575789c9ce82dbd0b183663367
S2799 <32448648+S2799@users.noreply.github.com>
2017-10-23 00:17:56 +0530

    SST

--- kiitattendance.weebly.com/server.js
+++ kiitattendance.weebly.com/server.js
@@ -6,11 +6,40 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var index = {
+
+	title='Home || Attendance Inspector';
+	heading='Home';
+	content = `abcd `;
+}
+function createTemplate (data) {
+	
+	var title = data.title;
+	var heading = data.heading;
+	var content = data.content;
+
+		var htmltemp =
+		`
+		 <!DOCTYPE html>
+		<html>
+		  <head>
+		    <meta charset="UTF-8">
+		    <title>${title}</title>
+		  </head>
+		  <body>
+		  ${content}
+		  </body>
+		</html>
+
+		`;
+return htmltemp;
+}
+
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, '/', 'index.html'));
+  res.send(createTemplate(index));
 });
 
-app.get('/ui/style.css', function (req, res) {
+app.get('/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, '/', 'style.css'));
 });
 
@@ -18,5 +47,5 @@ app.get('/ui/style.css', function (req, res) {
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
-  console.log(`IMAD course app listening on port ${port}!`);
+  console.log(`Attendance App listening on port ${port}!`);
 });
