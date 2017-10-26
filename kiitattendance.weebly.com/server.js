eaed0c735864c10d5b515769fe5fb4253fef74f4
S2799 <32448648+S2799@users.noreply.github.com>
2017-10-26 22:22:06 +0530

    changes

--- kiitattendance.weebly.com/server.js
+++ kiitattendance.weebly.com/server.js
@@ -8,13 +8,12 @@ app.use(morgan('combined'));
 var index = {
 
 	title:'Home || Attendance Inspector',
-	heading:'Home',
 	content:`
 			<ul class="menu">
-			<li class="menu"><a class="active" href="#home">Home</a></li>
-			<li class="menu"><a class="menu2" href="#Login">Login</a></li>
-			<li class="menu"><a class="menu2" href="#signup">Sign Up</a></li>
-			<li class="menu"><a class="menu2" href="#about">About</a></li>
+			<li class="menu"><a class="active" href="/">Home</a></li>
+			<li class="menu"><a class="menu2" href="/login">Login</a></li>
+			<li class="menu"><a class="menu2" href="/signup">Sign Up</a></li>
+			<li class="menu"><a class="menu2" href="/about">About</a></li>
 			</ul>
 		<br>
 		<br>
@@ -27,13 +26,12 @@ var index = {
 		<h1 class="head">
 		Welcome to KIIT Attendance Inspector
 	  </h1><br>
-		<button class="button, button1" onclick="window.location.href='#login'"><h3>Login</h3></button>
+		<button class="button, button1" onclick="window.location.href='/login'"><h3>Login</h3></button>
 		<button class="button, button1" onclick="window.location.href='/signup'"><h3>Sign Up</h3></button> `
 }
 function createTemplate (data) {
 
 	var title = data.title;
-	var heading = data.heading;
 	var content = data.content;
 
 		var htmltemp =
@@ -55,6 +53,15 @@ return htmltemp;
 
 app.get('/', function (req, res) {
   res.send(createTemplate(index));
+});
+app.get('/login', function (req, res) {
+  res.send(createTemplate(login));
+});
+app.get('/signup', function (req, res) {
+  res.send(createTemplate(signup));
+});
+app.get('/about', function (req, res) {
+  res.send(createTemplate(about));
 });
 
 app.get('/defstyle.css', function (req, res) {
