aabe037002de24158b83a8f562ae2e0bce5e59ea
S2799 <32448648+S2799@users.noreply.github.com>
2017-10-26 22:05:41 +0530

    SST for index success

--- kiitattendance.weebly.com/server.js
+++ kiitattendance.weebly.com/server.js
@@ -1,4 +1,3 @@
-
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
@@ -8,29 +7,48 @@ app.use(morgan('combined'));
 
 var index = {
 
-	title='Home || Attendance Inspector';
-	heading='Home';
-	content = `abcd `;
+	title:'Home || Attendance Inspector',
+	heading:'Home',
+	content:`
+			<ul class="menu">
+			<li class="menu"><a class="active" href="#home">Home</a></li>
+			<li class="menu"><a class="menu2" href="#Login">Login</a></li>
+			<li class="menu"><a class="menu2" href="#signup">Sign Up</a></li>
+			<li class="menu"><a class="menu2" href="#about">About</a></li>
+			</ul>
+		<br>
+		<br>
+		<br>
+		<br>
+		<br>
+		<br>
+		<br>
+		<br>
+		<h1 class="head">
+		Welcome to KIIT Attendance Inspector
+	  </h1><br>
+		<button class="button, button1" onclick="window.location.href='#login'"><h3>Login</h3></button>
+		<button class="button, button1" onclick="window.location.href='/signup'"><h3>Sign Up</h3></button> `
 }
 function createTemplate (data) {
-	
+
 	var title = data.title;
 	var heading = data.heading;
 	var content = data.content;
 
 		var htmltemp =
 		`
-		 <!DOCTYPE html>
-		<html>
-		  <head>
-		    <meta charset="UTF-8">
-		    <title>${title}</title>
-		  </head>
-		  <body>
-		  ${content}
-		  </body>
-		</html>
-
+			 <!DOCTYPE html>
+			<html>
+			  <head>
+			    <meta charset="UTF-8">
+			    <title>${title}</title>
+			  </head>
+				<link rel="stylesheet" type="text/css" href="defstyle.css">
+			  <body class="bckimg">
+			  ${content}
+			  </body>
+			</html>
 		`;
 return htmltemp;
 }
@@ -39,8 +57,8 @@ app.get('/', function (req, res) {
   res.send(createTemplate(index));
 });
 
-app.get('/style.css', function (req, res) {
-  res.sendFile(path.join(__dirname, '/', 'style.css'));
+app.get('/defstyle.css', function (req, res) {
+  res.sendFile(path.join(__dirname, '/', 'defstyle.css'));
 });
 
 
