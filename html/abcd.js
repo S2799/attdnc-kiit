59b800a560a5438626231fffa537d6f6ceda2472
S2799 <32448648+S2799@users.noreply.github.com>
2017-11-02 00:14:49 +0530

    trials

--- html/abcd.js
+++ html/abcd.js
@@ -10,3 +10,11 @@ con.connect(function(err) {
   if (err) throw err;
   console.log("Connected!");
 });
+con.query("use attdnc", function (err, result) {
+    if (err) throw err;
+    console.log("Database used");
+});
+con.query("SELECT * FROM profiles;", function (err, result, fields) {
+      if (err) throw err;
+      console.log(result)
+});
